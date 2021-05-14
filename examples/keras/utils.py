b87a78faef4a0e425a64f46d79ceac084a728912
tabeareichmann <tabea.reichmann@outlook.com>
2021-05-14 17:11:37 +0200

    add Lenet

--- examples/keras/utils.py
+++ examples/keras/utils.py
@@ -11,6 +11,7 @@ Utility functions and classes for MorphNet model zoo.
 import tensorflow as tf
 import numpy as np
 from tqdm import tqdm, trange
+from tensorflow.python.keras.layers import VersionAwareLayers
 
 
 def select_keras_base_model(base_model_name):
@@ -63,70 +64,35 @@ def select_keras_base_model(base_model_name):
     elif base_model_name == "NASNetMobile":
         base_model = tf.keras.applications.nasnet.NASNetMobile
     elif base_model_name == "LeNet":
-        def create_LeNet() -> tf.keras.Model:
-            """
-            Returns uncompiled LeNet5 model.
-            :param: IMG_SHAPE: Tuple[int, int, int]
-            :return: tf.keras.Model
-            """
-            Convolution2D = tf.keras.layers.Convolution2D
-            MaxPooling2D = tf.keras.layers.MaxPooling2D
-            Flatten = tf.keras.layers.Flatten
-            Dense = tf.keras.layers.Dense
-
-            model = tf.keras.models.Sequential()
-
-            with tf.name_scope("LeNet5"):
-                with tf.name_scope("Convolution_Block"):
-                    # Add the first convolution layer
-                    model.add(Convolution2D(
-                        filters=20,
-                        kernel_size=(5, 5),
-                        padding="same",
-                        input_shape=[28,28,1],
-                        activation="relu",
-                        name="Conv1"))
-
-                    # Add a pooling layer
-                    model.add(MaxPooling2D(
-                        pool_size=(2, 2),
-                        strides=(2, 2),
-                        name="MaxPool1"))
-
-                    # Add the second convolution layer
-                    model.add(Convolution2D(
-                        filters=50,
-                        kernel_size=(5, 5),
-                        padding="same",
-                        activation="relu",
-                        name="Conv2"))
-
-                    # Add a second pooling layer
-                    model.add(MaxPooling2D(
-                        pool_size=(2, 2),
-                        strides=(2, 2),
-                        name="MaxPool2"))
-
-                # Flatten the network
-                model.add(Flatten())
-
-                with tf.name_scope("Dense_Block"):
-                    # Add a fully-connected hidden layer
-                    model.add(Dense(500,
-                                    activation="relu",
-                                    name="Dense3"))
-
-                    # Add a fully-connected output layer
-                    model.add(Dense(10,
-                                    activation="softmax",
-                                    name="Dense4"))
-            return model
 
 
-        base_model = create_LeNet()
-        
+            #model = tf.keras.models.Sequential()
+        x=layers.Conv2D(filters=20,
+            kernel_size=(5, 5),
+            padding="same",
+            input_shape=[28,28,1],
+            activation="relu")
+        x=layers.MaxPooling2D(
+            pool_size=(2, 2),
+            strides=(2, 2))
+        x=layers.Conv2D(
+            filters=50,
+            kernel_size=(5, 5),
+            padding="same",
+            activation="relu")
+        x=layers.MaxPooling2D(
+            pool_size=(2, 2),
+            strides=(2, 2))
+        x=layers.Flatten()
+        x=layers.Dense(500,
+            activation="relu")
 
-        base_model.summary()
+        x=layers.Dense(10,
+            activation="softmax")
+
+
+        base_model = training.Model(inputs, x, name='mobilenet_%0.2f_%s' % (alpha, rows))
+
     else:
         raise Exception("Unsupported Base Model!")
 
